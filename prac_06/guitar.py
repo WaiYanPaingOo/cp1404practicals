43bea9ab806206a36a4f45299450073312e930fb
Wai Yan Paing Oo <waiyanpaingoo@my.jcu.edu.au>
2019-09-05 20:36:11 +0800

    "Commiting prac_06"

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -17,7 +17,6 @@ class Guitar:
         return age
 
     def is_vintage(self):
-
         if self.get_age() >= 50:
             return True
         else:
--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -13,9 +13,6 @@ def main():
 
     guitars = []
 
-    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
-
     print("My Guitars!")
     name = input("Name: ")
     while len(name) > 0:
